cda69369540225f0b409715938e6a8d6bf8ef0fe
schuylerbaer <schuylerbaer@gmail.com>
2026-02-03 12:11:03 -0700

    Changed users column hashed_pw to hashed_password

--- database/models.py
+++ database/models.py
@@ -48,7 +48,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    hashed_pw = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
 
     is_admin = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
