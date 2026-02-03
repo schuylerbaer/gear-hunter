1617e1b818b204b61a74996d9348a94b127e5676
schuylerbaer <schuylerbaer@gmail.com>
2026-02-03 11:11:53 -0700

    Add users table to db model

--- database/models.py
+++ database/models.py
@@ -19,7 +19,7 @@ class Alert(Base):
     __tablename__ = "alerts"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_email = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
     target_brand = Column(String)
     target_model = Column(String)
@@ -27,6 +27,8 @@ class Alert(Base):
 
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="alerts")
 
 class NotificationLog(Base):
     __tablename__ = "notification_logs"
@@ -40,3 +42,15 @@ class NotificationLog(Base):
 
     alert = relationship("Alert")
     alert = relationship("Listing")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_pw = Column(String, nullable=False)
+
+    is_admin = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    alerts = relationship("Alert", back_populates="user")
