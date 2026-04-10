fdc064750a71b90e3d0d77ba6eb29003b237cff0
schuylerbaer <schuylerbaer@gmail.com>
2026-04-10 00:35:24 -0600

    Configure supabase to use session storage

--- frontend/src/services/supabaseClient.ts
+++ frontend/src/services/supabaseClient.ts
@@ -3,4 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+      storage: window.sessionStorage,
+    }
+})
